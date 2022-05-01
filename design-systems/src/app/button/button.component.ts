861894b13a628f42a4d4c7411fc9e4286f283976
Andreas Wissel <github@kaeku.io>
2022-05-01 18:38:55 +0200

    lab-6

--- design-systems/src/app/button/button.component.ts
+++ design-systems/src/app/button/button.component.ts
@@ -2,6 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 
 export type ButtonType = 'primary' | 'secondary';
 
+/**
+ * This is the button component. It can be used to build clicky things!
+ *
+ * See [Figma reference](https://www.figma.com/file/X1JRLUCEp6JnoeKAxRPNeF/Angular-Architects-Design-Systems?node-id=1%3A3) for specifications.
+ */
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
--- design-systems/src/app/button/button.module.ts
+++ design-systems/src/app/button/button.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button.component';
 
-
-
 @NgModule({
-  declarations: [
-    ButtonComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ButtonComponent],
+  imports: [CommonModule],
+  exports: [ButtonComponent],
 })
-export class ButtonModule { }
+export class ButtonModule {}
