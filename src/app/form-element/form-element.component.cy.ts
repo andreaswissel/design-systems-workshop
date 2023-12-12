5f399c9d625ad9a00414b8c0e9eeede28afba271
Andreas Wissel <business@andreaswissel.com>
2023-12-12 21:28:57 +0100

    lab-9 solution

---
+++ src/app/form-element/form-element.component.cy.ts
@@ -0,0 +1,7 @@
+import { FormElementComponent } from './form-element.component'
+
+describe('FormElementComponent', () => {
+  it('should mount', () => {
+    cy.mount(FormElementComponent)
+  })
+})
---
+++ src/app/input/input.component.cy.ts
@@ -0,0 +1,7 @@
+import { InputComponent } from './input.component'
+
+describe('InputComponent', () => {
+  it('should mount', () => {
+    cy.mount(InputComponent)
+  })
+})
