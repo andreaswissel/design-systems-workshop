3cfd25e4feeb7938b478a8232a83b47268f1a198
Andreas Wissel <business@andreaswissel.com>
2024-04-14 15:22:44 +0200

    lab-2

---
+++ design-systems/src/app/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  @Input() public label: string = 'Button Label';
+}
